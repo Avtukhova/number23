37852ebeda50c402c6b6d76088d9db13ba0a1a08
Larisa Avtukhova <aln1967@yandex.ru>
2021-10-03 19:45:16 +0300

    Changes CallbackTest.java

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,9 +1,12 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
     private WebDriver driver;
@@ -19,15 +22,25 @@ public class CallbackTest {
         driver = new ChromeDriver();
     }
 
+     @Test
+    void shouldTestV1() {
+        driver.get("http://localhost:9999");
+        List<WebElement> inputField = driver.findElements(By.className("input__control"));
+        inputField.get(0).sendKeys("Василий");
+        inputField.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__text")).click();
+        driver.findElement(By.tagName("button")).click();
+        String actualMessage = driver.findElement(By.className("paragraph")).getText();
+        String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedMessage, actualMessage.trim());
+
+
+    }
+
     @AfterEach
     void tearDown() {
         driver.quit();
         driver = null;
-    }
-
-    @Test
-    void shouldTestV1() {
-        driver.get("http://localhost:9999");
     }
 
 

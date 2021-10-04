972a0685ff29fbc7dba3df579723cb969f88624c
Larisa Avtukhova <aln1967@yandex.ru>
2021-10-04 18:17:12 +0300

    Changes files

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
